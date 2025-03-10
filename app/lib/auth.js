d36d62e7adabc914317370832e6bc4217e543656
AYUSHMAAN1812 <ayushmaan2005dav@gmail.com>
2025-03-10 18:15:43 +0530

    added authentication

---
+++ app/lib/auth.js
@@ -0,0 +1,26 @@
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebaseConfig";
+
+const auth = getAuth(app);
+
+// Sign Up
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
+
+// Sign In
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
--- app/login/page.js
+++ app/login/page.js
@@ -1,19 +1,37 @@
-import React from 'react'
+"use client"
+
+import React,{useState} from 'react'
+import { signIn } from '../lib/auth'
 
 const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handelSignIn = async (event) => 
+  {
+    event.preventDefault();
+    try{
+      await signIn(email,password);
+      alert("Log In successful!");
+    }catch(error)
+    {
+      setError(error.message)
+    }
+  };
   return (
     <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] flex justify-center my-auto">
         <div className='flex flex-col justify-center align-center gap-8'>
             <div className='flex gap-2 text-2xl'>
                 <span > Email ID:</span>
-                <span className='border-1 border-white rounded-sm pl-4'> <input type="text" placeholder='Email ID'/></span>
+                <span className='border-1 border-white rounded-sm pl-4'> <input type="text" placeholder='Email ID' onChange={(e)=>{setEmail(e.target.value)}} required/></span>
             </div>
             <div className='flex gap-2 text-2xl'>
                 <span> Password:</span>
-                <span className='border-1 border-white rounded-sm pl-4'> <input type="text" placeholder='Enter your Password'/></span>
+                <span className='border-1 border-white rounded-sm pl-4'> <input type="text" placeholder='Enter your Password' onChange={(e)=>{setPassword(e.target.value)}}/></span>
             </div>
         </div>
-      
+      <button onClick={handelSignIn}>Sign In</button>
     </div>
   )
 }
