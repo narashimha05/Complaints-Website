ce09f0cecc4944ab9847780428ae0a36f1ceb5db
Prakash97971 <prakashshaweves270.com>
2025-03-12 12:15:23 +0000

    <message>

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -97,38 +97,31 @@ const Dashboard = () => {
   }
   const handleAdd = async (e) => {
     e.preventDefault();
-    const added = await addDataToFireStore(form.name, form.email, form.hostelName, form.hostelRoom, form.description, form.issue);
+    const added = await addDataToFireStore(
+      form.name,
+      form.email,
+      form.hostelName,
+      form.hostelRoom,
+      form.description,
+      form.issue
+    );
     if (added) {
-      
       alert("Complaint has been logged!");
-      const formData = new FormData();
-    formData.append("name", form.name);
-    formData.append("email", form.email);
-    formData.append("hostelName", form.hostelName);
-    formData.append("hostelRoom", form.hostelRoom);
-    formData.append("description", form.description);
-    formData.append("issue", form.issue);
-
-    try {
-      const response = await fetch(
-        "https://script.google.com/a/macros/iith.ac.in/s/AKfycbwnsbpxFrsgmcH1xCZ4KwHDmat_Oeelrbn-xXTm4JUjsrEQnFLnGdpc0raYb0K75qcA_Q/exec",
-        {
-          method: "POST",
-          body: formData,
+      try {
+        const response = await fetch("https://script.google.com/a/macros/iith.ac.in/s/AKfycbzIGaBFyHqiyfy02P50NTuswcBveEeelQ3_J-OR4_euuVxF9ofzl5aS8--a_pFKQBgTAQ/exec");
+        const data = await response.json();
+        console.log("Response from Apps Script:", data);
+        if (response.ok) {
+          alert("Complaint email has been sent successfully!");
+        } else {
+          alert("Error submitting the complaint!");
         }
-      );
-
-      if (response.ok) {
-        alert("Complaint has been logged successfully!");
-      } else {
-        alert("Error submitting the complaint!");
+      } catch (error) {
+        console.error("Error submitting form to Google Apps Script:", error);
       }
-    } catch (error) {
-      console.error("Error submitting form to Google Apps Script:", error);
     }
-
-    }
-  }
+  };
+  
   return (
     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <Navbar />
