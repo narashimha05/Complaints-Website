85450cf27093e5c8608f8614123e1073f71eb063
Prakash97971 <prakashshaweves270.com>
2025-03-12 19:24:50 +0000

    <message>

--- app/my_issues/page.js
+++ app/my_issues/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { db } from "../firebaseConfig";
-import { collection, getDocs, query, where, doc, deleteDoc, updateDoc,getDoc } from "firebase/firestore";
+import { collection, getDocs, query, where, doc, deleteDoc, updateDoc, getDoc } from "firebase/firestore";
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Link from "next/link";
@@ -55,109 +55,110 @@ const Issues = () => {
 
   const handleDelete = async (docId, e) => {
     e.preventDefault();
-  
+
     // Open the Google Apps Script URL immediately to ensure it's triggered by the user action.
-    
+
     try {
       // Get the reference to the document in Firestore and update the 'resolved' field
       const docRef = doc(db, "one", docId);
       await updateDoc(docRef, { resolved: true });
-      
+
       // Log to check if 'resolved' is being updated in Firestore
       const updatedDoc = await getDoc(docRef);
       console.log("Document after update:", updatedDoc.data());
-      
+
       // Inform the user that the complaint has been resolved
       toast('Complaint has been Resolved!', {
-                            position: "top-right",
-                            autoClose: 1000,
-                            hideProgressBar: false,
-                            closeOnClick: false,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "dark",
-                            transition: Bounce,
-                        });
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
       setStatus("Done");
-      
+
       window.open("https://script.google.com/a/macros/iith.ac.in/s/AKfycbzfPttcSOBIEnvVdTEgXCSd69To1fbn0xatjQc9FIBw_L6RwsOVG8h6oCLQI4jvEcZP/exec");
-      
-  
+
+
     } catch (error) {
       console.error("Error updating or deleting document: ", error);
     }
   };
 
   return <>
-   <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={false}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-              transition={Bounce}
-          />
-    <div className="absolute inset-0 -z-10 h-full w-full px-2 py-12 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] items-center">
-  <div className="flex justify-center items-start mt-10 bg-transparent max-h-[80vh] max-w-[90vw] overflow-auto">
-    <table className="w-full max-w-[95vw] md:max-w-[80vw] lg:max-w-[60vw] mx-auto divide-y divide-x divide-gray-100 border border-gray-200 bg-transparent text-white z-20">
-      <thead>
-        <tr>
-          <th scope="col" className="px-2 py-2 text-left text-xs md:text-sm font-medium uppercase tracking-wider">
-            Issue
-          </th>
-          <th scope="col" className="px-2 py-2 text-center text-xs md:text-sm font-medium uppercase tracking-wider">
-            Issue Description
-          </th>
-          <th scope="col" className="px-2 py-2 text-right text-xs md:text-sm font-medium uppercase tracking-wider pl-2">
-            View Mail
-          </th>
-          <th scope="col" className="px-2 py-2 text-right text-xs md:text-sm font-medium uppercase tracking-wider pl-2">
-            Resolved?
-          </th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-x divide-gray-200">
-        {userdata.length === 0 ? (
-          <tr>
-            <td colSpan="3" className="px-2 py-4 text-center text-white">
-              No unresolved issues found.
-            </td>
-          </tr>
-        ) : (
-          userdata.map((problem, index) => (
-            <tr key={index}>
-              <td className="px-2 py-2 text-left text-xs md:text-sm font-normal text-white break-words">
-                {problem.issue}
-              </td>
-              <td className="px-2 py-2 align-top text-left whitespace-normal break-words text-xs md:text-sm font-normal text-white">
-                {problem.description}
-              </td>
-              <td className="px-2 py-2 align-top text-left whitespace-normal break-words text-xs md:text-sm font-normal text-white underline">
-                <Link href = {problem.threadID} target="_blank">View</Link>
-              </td>
-              <td className="px-2 py-2 text-right text-xs md:text-sm font-normal text-black">
-                <button
-                  className="hover:cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-1 overflow-hidden text-xs md:text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300"
-                  onClick={(e) => handleDelete(problem.id, e)}
-                >
-                  <span className="relative px-3 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent">
-                    {status}
-                  </span>
-                </button>
-              </td>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
+    <div className="absolute inset-0 -z-10 h-full w-full px-2 py-12 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] flex items-center justify-center">
+      <div className="bg-transparent max-h-[80vh] max-w-[90vw] overflow-auto">
+        <table className="max-w-[95vw] md:max-w-[80vw] lg:max-w-[60vw] divide-y divide-x divide-gray-100 border border-gray-200 bg-transparent text-white">
+
+          <thead>
+            <tr>
+              <th scope="col" className="px-2 py-2 text-left text-xs md:text-sm font-medium uppercase tracking-wider">
+                Issue
+              </th>
+              <th scope="col" className="px-2 py-2 text-center text-xs md:text-sm font-medium uppercase tracking-wider">
+                Issue Description
+              </th>
+              <th scope="col" className="px-2 py-2 text-right text-xs md:text-sm font-medium uppercase tracking-wider pl-2">
+                View Mail
+              </th>
+              <th scope="col" className="px-2 py-2 text-right text-xs md:text-sm font-medium uppercase tracking-wider pl-2">
+                Resolved?
+              </th>
             </tr>
-          ))
-        )}
-      </tbody>
-    </table>
-  </div>
-</div>
+          </thead>
+          <tbody className="divide-y divide-x divide-gray-200">
+            {userdata.length === 0 ? (
+              <tr>
+                <td colSpan="3" className="px-2 py-4 text-center text-white">
+                  No unresolved issues found.
+                </td>
+              </tr>
+            ) : (
+              userdata.map((problem, index) => (
+                <tr key={index}>
+                  <td className="px-2 py-2 text-left text-xs md:text-sm font-normal text-white break-words">
+                    {problem.issue}
+                  </td>
+                  <td className="px-2 py-2 align-top text-left whitespace-normal break-words text-xs md:text-sm font-normal text-white">
+                    {problem.description}
+                  </td>
+                  <td className="px-2 py-2 align-top text-left whitespace-normal break-words text-xs md:text-sm font-normal text-white underline">
+                    <Link href={problem.threadID} target="_blank">View</Link>
+                  </td>
+                  <td className="px-2 py-2 text-right text-xs md:text-sm font-normal md:text-black text-white">
+                    <button
+                      className="hover:cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-1 overflow-hidden text-xs md:text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300"
+                      onClick={(e) => handleDelete(problem.id, e)}
+                    >
+                      <span className="relative px-3 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent">
+                        {status}
+                      </span>
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
 
   </>
 };
