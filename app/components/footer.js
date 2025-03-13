53d00e618e2a535c57b089d263a37f754ab0393d
Prakash97971 <prakashshaweves270.com>
2025-03-13 16:50:06 +0000

    <message>

--- app/components/footer.js
+++ app/components/footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <div className='flex h-10 mb-4 md:mb-0  bg-transparent absolute bottom-0 left-0  w-full gap-2 pl-3.5 justify-center items-center'>
-          <div className='mt-2 flex gap-8 text-white font-medium justify-center items-center' >
+    <div className='flex h-10 mb-1 md:mb-0  bg-transparent absolute bottom-0 left-0  w-full gap-2 pl-3.5 justify-center items-center'>
+          <div className='md:mt-4 mt-5 flex gap-8 text-white font-medium justify-center items-center ' >
             <div className='my-auto '> Copyright @2025</div>
           <Link className="my-auto py-auto" href={"/reportissue"}>
                 <div className=""> Report an Issue</div>
