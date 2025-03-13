c0d18c54089e12682f0ae9d9141bd83979b2f278
AYUSHMAAN1812 <ayushmaan2005dav@gmail.com>
2025-03-14 00:12:29 +0530

    message

--- app/my_issues/page.js
+++ app/my_issues/page.js
@@ -164,7 +164,7 @@ const Issues = () => {
                   </td>
                   <td className="px-2 py-2 text-right text-xs md:text-sm font-normal ">
                     <button
-                      className="text-white hover:cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-1 overflow-hidden text-xs md:text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 
+                      className=" hover:cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-1 overflow-hidden text-xs md:text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 
     "
                       onClick={(e) => { toggleDarkMode(); handleDelete(problem.id, e); }}
 
